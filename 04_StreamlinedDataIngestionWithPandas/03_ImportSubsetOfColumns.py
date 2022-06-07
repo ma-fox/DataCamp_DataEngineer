65e050597056cc560ddab43d99bd6c4157dba536
marco fox <marco.a.fox@gmail.com>
2022-06-07 10:17:30 +0200

    Import a subset of columns

--- 04_StreamlinedDataIngestionWithPandas/03_ImportSubsetOfColumns.py
+++ 04_StreamlinedDataIngestionWithPandas/03_ImportSubsetOfColumns.py
@@ -1,8 +1,11 @@
+# Import a subset of columns
+
 # Create list of columns to use
-cols = ____
+cols = ['zipcode', 'agi_stub', 'mars1', 'MARS2', 'NUMDEP']
 
 # Create dataframe from csv using only selected columns
-data = ____("vt_tax_data_2016.csv", ____)
+data = pd.read_csv("vt_tax_data_2016.csv", usecols=cols)
 
 # View counts of dependents and tax returns by income level
 print(data.groupby("agi_stub").sum())
+# print(data)
