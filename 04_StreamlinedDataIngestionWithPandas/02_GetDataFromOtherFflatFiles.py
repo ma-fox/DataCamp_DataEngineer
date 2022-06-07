aca55ffb36a3a7c2a9352333baa4dab507751d81
marco fox <marco.a.fox@gmail.com>
2022-06-07 10:14:12 +0200

    Get data from other flat files

--- 04_StreamlinedDataIngestionWithPandas/02_GetDataFromOtherFflatFiles.py
+++ 04_StreamlinedDataIngestionWithPandas/02_GetDataFromOtherFflatFiles.py
@@ -1,10 +1,18 @@
+# Get data from other flat files
+
 # Import pandas with the alias pd
-____
+import pandas as pd
+
+# Import Matplotlib with the alias plt
+from matplotlib import pyplot as plt
+
 
 # Load TSV using the sep keyword argument to set delimiter
-data = ____(____, ____)
+# TSV is tab separated values, so we use sep='\t'
+data = pd.read_csv("vt_tax_data_2016.tsv", sep="\t")
 
 # Plot the total number of tax returns by income group
+# groupby() is a method of DataFrame, it groups by a column
 counts = data.groupby("agi_stub").N1.sum()
 counts.plot.bar()
 plt.show()
