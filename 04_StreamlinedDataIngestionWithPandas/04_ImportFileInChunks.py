a04ec3a0ecab4f9412488aef9b6e12acb84b7a9d
marco fox <marco.a.fox@gmail.com>
2022-06-08 15:39:28 +0200

    Added 04_StreamlinedDataIngestionWithPandas lessons content

---
+++ 04_StreamlinedDataIngestionWithPandas/04_ImportFileInChunks.py
@@ -0,0 +1,22 @@
+# Import a file in chunks
+
+# Import pandas with the alias pd
+import pandas as pd
+
+# Create dataframe of next 500 rows with labeled columns
+col_names = list(vt_data_first500)
+
+vt_data_next500 = pd.read_csv("vt_tax_data_2016.csv",
+                              nrows=500,
+                              skiprows=500,
+                              header=None,
+                              names=col_names)
+
+# nrow = number of rows to read
+# skiprows = number of rows to skip
+# header = None to skip header, since pandas itself will otherwise create a wrong header
+# names = list of column names, defined above in col_names
+
+# View the Vermont dataframes to confirm they're different
+print(vt_data_first500.head())
+print(vt_data_next500.head())
