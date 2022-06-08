b1233cbb675574c9ff62031c0f7290ac42b32ef9
marco fox <marco.a.fox@gmail.com>
2022-06-08 15:57:42 +0200

    Select a single sheet in an excel file

---
+++ 04_StreamlinedDataIngestionWithPandas/02_ImportingDataFromExcelFiles/03_SelectSingleSheet.py
@@ -0,0 +1,19 @@
+# Create a dataframe from the second workbook sheet by passing the sheet's position to sheet_name.
+
+# Create df from second worksheet by referencing its position
+responses_2017 = pd.read_excel("fcc_survey.xlsx", sheet_name=1)
+
+# Graph where people would like to get a developer job
+job_prefs = responses_2017.groupby("JobPref").JobPref.count()
+job_prefs.plot.barh()
+plt.show()
+
+# Create a dataframe from the 2017 sheet by providing the sheet's name to read_excel().
+
+# Create df from second worksheet by referencing its name
+responses_2017 = pd.read_excel("fcc_survey.xlsx", sheet_name='2017')
+
+# Graph where people would like to get a developer job
+job_prefs = responses_2017.groupby("JobPref").JobPref.count()
+job_prefs.plot.barh()
+plt.show()
