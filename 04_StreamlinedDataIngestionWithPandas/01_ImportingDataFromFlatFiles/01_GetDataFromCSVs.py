3b61de8621e84d697ab94dd4cfed9001dc8e6f7f
marco fox <marco.a.fox@gmail.com>
2022-06-08 15:49:22 +0200

    Restructioning First chapter

---
+++ 04_StreamlinedDataIngestionWithPandas/01_ImportingDataFromFlatFiles/01_GetDataFromCSVs.py
@@ -0,0 +1,10 @@
+# Get data from CSVs
+
+# Import pandas as pd
+import pandas as pd
+
+# Read the CSV and assign it to the variable data
+data = pd.read_csv("vt_tax_data_2016.csv")
+
+# View the first few lines of data
+print(data.head())
