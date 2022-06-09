2cf85f61d571e20ec0e8a4da07a8ca47837c18d6
marco fox <marco.a.fox@gmail.com>
2022-06-09 15:26:51 +0200

    Filtering on multiple conditions

---
+++ 04_StreamlinedDataIngestionWithPandas/02_ImportingDataFromExcelFiles/11_FilteringOnMultipleConditions.py
@@ -0,0 +1,18 @@
+# Filtering on multiple conditions
+
+# Import pandas with the alias pd
+import pandas as pd
+
+# Create query for records with max temps <= 32 or snow >= 1
+query = """
+SELECT *
+  FROM weather
+  WHERE tmax <= 32
+  OR snow >= 1;
+"""
+
+# Query database and assign result to wintry_days
+wintry_days = pd.read_sql(query, engine)
+
+# View summary stats about the temperatures
+print(wintry_days.describe())
