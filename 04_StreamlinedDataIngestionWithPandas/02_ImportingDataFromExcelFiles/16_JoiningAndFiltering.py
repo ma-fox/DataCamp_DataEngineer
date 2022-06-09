d55197231714dd81280771259ec7f751a5679b5f
marco fox <marco.a.fox@gmail.com>
2022-06-09 15:36:53 +0200

    Joining and filtering

---
+++ 04_StreamlinedDataIngestionWithPandas/02_ImportingDataFromExcelFiles/16_JoiningAndFiltering.py
@@ -0,0 +1,18 @@
+# Joining and filtering
+
+# Import pandas with the alias pd
+import pandas as pd
+
+# Query to get water leak calls and daily precipitation
+query = """
+SELECT hpd311calls.*, weather.prcp
+  FROM hpd311calls
+  JOIN weather
+    ON hpd311calls.created_date = weather.date
+  WHERE hpd311calls.complaint_type = 'WATER LEAK';"""
+
+# Load query results into the leak_calls dataframe
+leak_calls = pd.read_sql(query, engine)
+
+# View the dataframe
+print(leak_calls.head())
