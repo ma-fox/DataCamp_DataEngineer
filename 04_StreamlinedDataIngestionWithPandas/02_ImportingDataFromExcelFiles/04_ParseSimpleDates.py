e5833b2822686dea2c517783fe80987da4f974ff
marco fox <marco.a.fox@gmail.com>
2022-06-09 15:13:20 +0200

    Parse simple dates

--- 04_StreamlinedDataIngestionWithPandas/02_ImportingDataFromExcelFiles/04_ParseSimpleDates.py
+++ 04_StreamlinedDataIngestionWithPandas/02_ImportingDataFromExcelFiles/04_ParseSimpleDates.py
@@ -1,9 +1,10 @@
+# Parse simple dates
+
 # Import pandas with the alias pd
 import pandas as pd
 
 # Load file, with Part1StartTime parsed as datetime data
-survey_data = pd.read_excel("fcc_survey.xlsx",
-                            parse_dates=)
+survey_data = pd.read_excel("fcc_survey.xlsx", parse_dates=['Part1StartTime'])
 
 # Print first few values of Part1StartTime
-print(survey_data.Part1StartTime.____)
+print(survey_data.Part1StartTime.head())
