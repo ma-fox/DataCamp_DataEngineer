70b9e2a4e28f7ffd404effab5d0d8a1c0e053f9b
marco fox <marco.a.fox@gmail.com>
2022-06-13 22:56:24 +0200

    Built-in practice: range() - unpacking a range object using the star character (*)

--- 05_WritingEfficientPythonCode/02_Built-inPractice_range.py
+++ 05_WritingEfficientPythonCode/02_Built-inPractice_range.py
@@ -13,3 +13,6 @@ print(nums_list)
 # Create a new list of odd numbers from 1 to 11 by unpacking a range object
 nums_list2 = [*range(1, 12, 2)]
 print(nums_list2)
+
+# Notice that using Python's range() function allows you to create a range object of numbers without explicitly typing them out.
+# You can convert the range object into a list by using the list() function or by unpacking it into a list using the star character (*).
