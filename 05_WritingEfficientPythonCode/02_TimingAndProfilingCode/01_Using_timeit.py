59112de52b57f33f72645e835ebc88aa05d60000
marco fox <marco.a.fox@gmail.com>
2022-06-14 12:11:29 +0200

    %timeit - works only in jupyter notebook or interpreter shell

---
+++ 05_WritingEfficientPythonCode/02_TimingAndProfilingCode/01_Using_timeit.py
@@ -0,0 +1,27 @@
+# Using %timeit: your turn!
+
+import numpy as np
+# testing using timeit(), which is only available in Python 3 using jupyter notebook!
+
+# Use list comprehension and range() to create a list of integers from 0 to 50 called nums_list_comp.
+
+# Create a list of integers (0-50) using list comprehension
+nums_list_comp = [num for num in range(51)]
+print(nums_list_comp)
+
+# Use range() to create a list of integers from 0 to 50 and unpack its contents into a list called nums_unpack.
+# Create a list of integers (0-50) using list comprehension
+nums_list_comp = [num for num in range(51)]
+print(nums_list_comp)
+
+# Create a list of integers (0-50) by unpacking range
+nums_unpack = [*range(51)]
+print(nums_unpack)
+
+# Create a list of integers (0-50) using list comprehension
+%timeit nums_list_comp = [num for num in range(51)]
+print(nums_list_comp)
+
+# Create a list of integers (0-50) by unpacking range
+%timeit nums_unpack = [*range(51)]
+print(nums_unpack)
