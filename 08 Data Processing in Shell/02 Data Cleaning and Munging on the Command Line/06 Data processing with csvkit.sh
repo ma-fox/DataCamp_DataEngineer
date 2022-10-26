c6616c627958536071059c1c88a20fcc08d4ffa3
marco fox <marco.a.fox@gmail.com>
2022-10-26 10:17:10 +0200

    Check to confirm name and location of data file

--- 08 Data Processing in Shell/02 Data Cleaning and Munging on the Command Line/06 Data processing with csvkit.sh
+++ 08 Data Processing in Shell/02 Data Cleaning and Munging on the Command Line/06 Data processing with csvkit.sh
@@ -1,2 +1,5 @@
 # Convert the Spotify201809 tab into its own csv file
 in2csv Spotify_201809_201810.xlsx --sheet "Spotify201809" > Spotify201809.csv
+
+# Check to confirm name and location of data file
+ls
