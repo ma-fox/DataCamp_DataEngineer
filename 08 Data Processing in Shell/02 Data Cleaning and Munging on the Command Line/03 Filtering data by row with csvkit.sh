832419577a097eef41eb103b48607b55454f519f
marco fox <marco.a.fox@gmail.com>
2022-10-26 10:10:10 +0200

    Filter for row(s) where track_id = 118GQ70Sp6pMqn6w1oKuki

--- 08 Data Processing in Shell/02 Data Cleaning and Munging on the Command Line/03 Filtering data by row with csvkit.sh
+++ 08 Data Processing in Shell/02 Data Cleaning and Munging on the Command Line/03 Filtering data by row with csvkit.sh
@@ -1,2 +1,5 @@
 # Print a list of column headers in the data
 csvcut -n Spotify_MusicAttributes.csv
+
+# Filter for row(s) where track_id = 118GQ70Sp6pMqn6w1oKuki
+csvgrep -c "track_id" -m 118GQ70Sp6pMqn6w1oKuki Spotify_MusicAttributes.csv
