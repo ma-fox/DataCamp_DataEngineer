eca9a65aa91edd7e312651f5adee3a14a3b00256
marco fox <marco.a.fox@gmail.com>
2022-10-27 13:09:49 +0200

    Print the first, third, and fifth column, by position

--- 08 Data Processing in Shell/02 Data Cleaning and Munging on the Command Line/02 Filtering data by column with csvkit.sh
+++ 08 Data Processing in Shell/02 Data Cleaning and Munging on the Command Line/02 Filtering data by column with csvkit.sh
@@ -3,3 +3,6 @@ csvcut -n Spotify_MusicAttributes.csv
 
 # Print the first column, by position
 csvcut -c 1 Spotify_MusicAttributes.csv
+
+# Print the first, third, and fifth column, by position
+csvcut -c 1,3,5 Spotify_MusicAttributes.csv
