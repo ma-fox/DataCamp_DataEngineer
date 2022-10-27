50ae76ef850d55b7eca42fd821fdf2d554c3f27e
marco fox <marco.a.fox@gmail.com>
2022-10-27 13:21:50 +0200

    Print a list of column headers in the data

--- 08 Data Processing in Shell/02 Data Cleaning and Munging on the Command Line/02 Filtering data by column with csvkit.sh
+++ 08 Data Processing in Shell/02 Data Cleaning and Munging on the Command Line/02 Filtering data by column with csvkit.sh
@@ -9,3 +9,6 @@ csvcut -c 1,3,5 Spotify_MusicAttributes.csv
 
 # Print the first column, by name
 csvcut -c "track_id" Spotify_MusicAttributes.csv
+
+# Print the track id, song duration, and loudness, by name
+csvcut -c "track_id","duration_ms","loudness" Spotify_MusicAttributes.csv
